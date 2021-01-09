1a50c1cb44d151e19f0bc32702036075d81bceb6
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-01-09 10:21:42 -0300

    fixed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('./pageHome/home.module').then(m => m.HomeModule) },
   {path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-  {path: 'blog', loadChildren: () => import('./page-blog/page-blog.module').then(m => m.PageBlogModule) }
+  {path: 'blog', loadChildren: () => import('./page-blog/page-blog.module').then(m => m.PageBlogModule) },
+  {path: '**', redirectTo:'home', pathMatch: 'full'},
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
