09c36d2ec6d5436a1251c608076b58910d686296
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-01-09 10:29:26 -0300

    pipe sanityzer urls

--- src/app/pipes/sanitezer-url.pipe.ts
+++ src/app/pipes/sanitezer-url.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Pipe({
   name: 'sanitizerUrl'
 })
 export class SanitizerUrlPipe implements PipeTransform {
   constructor(private domSeguro: DomSanitizer) {}
 
-  transform( url: string , value: string): any {
+  transform( url: string , value: string): SafeResourceUrl {
     return this.domSeguro.bypassSecurityTrustResourceUrl(url + value);
   }
 
