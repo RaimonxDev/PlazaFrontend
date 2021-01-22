0531336911a9e0a560432c7702c5d581940629cc
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-01-22 10:55:42 -0300

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
+  nameWeb: 'plazafrontend.io',
   urlImages: "http://localhost:1337",
-  urlSites :"http://localhost",
+  urlSites : "http://localhost",
   portSites: 1337,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  nameWeb: 'plazafrontend.io',
   urlImages: "http://localhost:1337",
   urlSites :"http://localhost",
   portSites: 1337,
