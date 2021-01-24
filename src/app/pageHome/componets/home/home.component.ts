42c5dede6eadd89f216b2ab511684763b07621e6
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-01-24 15:41:09 -0300

    home services added

--- src/app/pageHome/componets/home/home.component.ts
+++ src/app/pageHome/componets/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SeoService } from "@shared/services/seo.service";
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+
+
+
+  constructor( private seo: SeoService) { }
 
   ngOnInit(): void {
+    this.seo.getTagForPage('home')
+  }
+
+
+  ngOnDestroy(){
+      this.seo.deleteMetaTags()
   }
 
 }
---
+++ src/app/pageHome/services/home.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { handleHttpResponseError } from '@utils/http-errors';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+
+  // private urlSites:string = environment.urlSites
+  // private portSites: number = environment.portSites
+  // constructor( private _http: HttpClient) { }
+
+
+
+
+
+}
