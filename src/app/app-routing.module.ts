1fd53c7d3e555c2be02e2d928250e38a0b884f57
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-02-08 10:31:42 -0300

    disable auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfileGuard } from '@shared/guards/profile/profile.guard';
+// import { ProfileGuard } from '@shared/guards/profile/profile.guard';
 
 const routes: Routes = [
   { path: '', redirectTo:'home', pathMatch: 'full'},
   { path: 'home', loadChildren:    () => import('./pageHome/home.module').then(m => m.HomeModule),data: {title:'home'} },
   { path: 'about', loadChildren:   () => import('./about/about.module').then(m => m.AboutModule),data: {title:'about'} },
   { path: 'blog', loadChildren:    () => import('./page-blog/page-blog.module').then(m => m.PageBlogModule),data: {title:'blog'} },
-  { path: 'auth', loadChildren:    () => import('./auth/auth.module').then(m => m.AuthModule),data: {title:'auth'} },
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),data: {title:'dashboard'},
-   canActivate: [ProfileGuard],
-   canLoad:     [ProfileGuard]
+  // { path: 'auth', loadChildren:    () => import('./auth/auth.module').then(m => m.AuthModule),data: {title:'auth'} },
+  // { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),data: {title:'dashboard'},
+  //  canActivate: [ProfileGuard],
+  //  canLoad:     [ProfileGuard]
 
-  },
+  // },
   { path: '**', redirectTo:'home', pathMatch: 'full'},
 
 ];
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,28 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 
-import { ReactiveFormsModule } from '@angular/forms'
-import { HttpClientModule } from '@angular/common/http';
-import { AuthService } from './services/auth.service';
+// import { ReactiveFormsModule } from '@angular/forms'
+// import { HttpClientModule } from '@angular/common/http';
+// import { AuthService } from './services/auth.service';
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-// import { ModalsModule } from "@shared/Modules/modals/modals.module";
-
-
-// ui
+// import { AuthRoutingModule } from './auth-routing.module';
+// import { LoginComponent } from './components/login/login.component';
+// import { RegisterComponent } from './components/register/register.component';
+// // import { ModalsModule } from "@shared/Modules/modals/modals.module";
 
 
-@NgModule({
-  declarations: [ LoginComponent, RegisterComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+// // ui
 
-  ],
-  providers: [AuthService]
-})
-export class AuthModule { }
+
+// @NgModule({
+//   declarations: [ LoginComponent, RegisterComponent],
+//   imports: [
+//     CommonModule,
+//     AuthRoutingModule,
+//     ReactiveFormsModule,
+//     HttpClientModule,
+
+//   ],
+//   providers: [AuthService]
+// })
+// export class AuthModule { }
