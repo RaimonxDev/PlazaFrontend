3d2a09277e07bcbc59a33caf1251f22822ad3e8b
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-02-13 22:35:19 -0300

    news variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,6 @@ export const environment = {
   urlImages: "http://localhost:1337",
   urlSites : "http://localhost",
   portSites: 1337,
+  endPointAllCategorys: 'http://localhost:1337/categories',
+  endPointCategory: 'http://localhost:1337/categories?name_category='
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,8 @@ export const environment = {
   urlImages: "http://localhost:1337",
   urlSites :"http://localhost",
   portSites: 1337,
+  endPointAllCategorys: 'http://localhost:1337/categories',
+  endPointCategory: 'http://localhost:1337/categories?name_category=deploy',
 };
 
 /*
