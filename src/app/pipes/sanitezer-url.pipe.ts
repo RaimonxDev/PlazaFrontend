6590ba582a0dfa6ec22b3a5d37c1df21d7e75c4c
Ramon Martinez <ramonmartinezcordero@gmail.com>
2021-02-27 22:22:03 -0300

    fixed url undefined

--- src/app/pipes/sanitezer-url.pipe.ts
+++ src/app/pipes/sanitezer-url.pipe.ts
@@ -1,7 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { environment } from '../../environments/environment';
-
 
 @Pipe({
   name: 'sanitizerUrl'
@@ -11,6 +9,11 @@ export class SanitizerUrlPipe implements PipeTransform {
   constructor(private domSeguro: DomSanitizer) {}
 
   transform( url: string , value: string): SafeResourceUrl {
+
+    // En produccion las url se guardan en AWS S3. Strapi devuelve la url completa
+    if(url === undefined  || url === ''){
+      return this.domSeguro.bypassSecurityTrustResourceUrl(value);
+    }
     return this.domSeguro.bypassSecurityTrustResourceUrl(url + value);
   }
 
